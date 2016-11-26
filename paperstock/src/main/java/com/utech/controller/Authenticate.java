3a444cf25803749c2ff6e636a83ee43c4baa765c
Maniarasan <utechnologies@hotmail.com>
2016-11-26 19:52:18 +0530

    Initial project creation

---
+++ paperstock/src/main/java/com/utech/controller/Authenticate.java
@@ -0,0 +1,32 @@
+package com.utech.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Authenticate
+ */
+@WebServlet(description = "To validate the user credentials", urlPatterns = { "/Authenticate" })
+public class Authenticate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Authenticate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
